2890354fd7a0c273f75e1740f27d95954f964fc7
DeanZahavy <91829842+DeanZahavy@users.noreply.github.com>
2023-11-08 10:05:39 +0200

    setup simple express service

---
+++ BE/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// GET endpoint
+app.get('/api/get', (req, res) => {
+    res.send('Hello, World! (GET)');
+});
+
+// POST endpoint
+app.post('/api/post', (req, res) => {
+    res.send('Hello, World! (POST)');
+});
+
+// PUT endpoint
+app.put('/api/put', (req, res) => {
+    res.send('Hello, World! (PUT)');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
