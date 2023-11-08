5dfa4fa960f13fedab06ec60e3b91853bd027849
DeanZahavy <91829842+DeanZahavy@users.noreply.github.com>
2023-11-08 12:27:02 +0200

    add codes

--- BE/server.js
+++ BE/server.js
@@ -1,25 +1,25 @@
 const express = require('express');
+const { StatusCodes } = require('http-status-codes');
+
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-// GET endpoint
-app.get('/api/get', (req, res) => {
-  res.send('Hello, World! (GET)');
+app.get('/api/get', (req, res) => {  
+  res.status(StatusCodes.OK).send('Hello, World! (GET)');
 });
 
-// POST endpoint
 app.post('/api/post', (req, res) => {
-  res.send('Hello, World! (POST)');
+  res.status(StatusCodes.OK).send('Hello, World! (POST)');
 });
 
-// PUT endpoint
 app.put('/api/put', (req, res) => {
-  res.send('Hello, World! (PUT)');
+  res.status(StatusCodes.OK).send('Hello, World! (PUT)');
 });
 
 const PORT = process.env.EXPORT_PORT || 3002;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
