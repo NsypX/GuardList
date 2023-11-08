a2ab451cd49d5c8d11213685ed48992ede2fbeb3
DeanZahavy <91829842+DeanZahavy@users.noreply.github.com>
2023-11-08 10:36:25 +0200

    setting docker file

--- BE/server.js
+++ BE/server.js
@@ -19,7 +19,7 @@ app.put('/api/put', (req, res) => {
     res.send('Hello, World! (PUT)');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.EXPORT_PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
