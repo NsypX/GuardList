64463d9003bbf8e708768ecd49f7dd1dc5cdf56d
DeanZahavy <91829842+DeanZahavy@users.noreply.github.com>
2023-11-10 13:18:49 +0200

    add tabs

--- FE/src/components/MainContainer.js
+++ FE/src/components/MainContainer.js
@@ -1,39 +1,40 @@
 import React from 'react';
-
-import { Typography, Divider, Image } from 'antd';
+import { Typography, Divider, Image, Tabs } from 'antd';
 import GuardsContainer from './GuardsContainer';
 import ShiftsContainer from './ShiftsContainer';
 
 const { Title } = Typography;
+const { TabPane } = Tabs;
 
 const MainContainer = () => {
-
   return (
     <>
       <Title level={1}>Guard App</Title>
 
-      <Divider/>
-      
-      <GuardsContainer/>
+      <Tabs defaultActiveKey="1" type="card">
+        <TabPane tab="Guards" key="1">
+          <GuardsContainer />
+        </TabPane>
+
+        <TabPane tab="Shifts" key="2">
+          <ShiftsContainer />
+        </TabPane>
+
+        <TabPane tab="Future Guard Schedule" key="3">
+          <Title level={2}>Future Guard Schedule</Title>      
+        </TabPane>
+      </Tabs>
 
       <Divider/>
 
-      <ShiftsContainer/>
-      
-      <Divider/>
+      <Title level={2}>{'MrBeast Love <3'}</Title>
 
-      <Title level={2}>Future Guard Schedule</Title>
-
-      <Divider/>   
-      
       <a href={'https://shopbeastphilanthropy.com/'} target="_blank" rel="noopener noreferrer">
-        <Image        
-          src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSrica07IIWQjW3svSkp4zN7mnCTwlm6kcKA&usqp=CAU'} 
-          alt="Mrbeast is amazing" // specify the alt text for the image
-        />
-      </a>
-
-      <Divider/>            
+         <Image
+           src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSrica07IIWQjW3svSkp4zN7mnCTwlm6kcKA&usqp=CAU'}
+           alt="Mrbeast is amazing" // specify the alt text for the image
+         />
+      </a>        
     </>
   );
 };
