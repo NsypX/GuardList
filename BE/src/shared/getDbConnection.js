51532d190985b252cfceb4ab6e037c5426fd8144
DeanZahavy <91829842+DeanZahavy@users.noreply.github.com>
2023-11-12 18:51:08 +0200

    commit

--- BE/src/shared/getDbConnection.js
+++ BE/src/shared/getDbConnection.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const getDbConnection = async () => {
+  console.log('DB_URI: ', process.env.DB_URI);
   const client = await MongoClient.connect(process.env.DB_URI || 'mongodb://localhost:27018/guards-db', {});
 
   const db = client.db();
